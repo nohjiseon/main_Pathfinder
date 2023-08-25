073f7046ee0147c1128bb0fe01c629ff7b15be92
minyongP <86906386+Minon98@users.noreply.github.com>
2023-08-25 17:41:28 +0900

    Feat: Add common code

---
+++ server/src/main/java/com/pathfinder/server/dto/SingleResponseDto.java
@@ -0,0 +1,10 @@
+package com.pathfinder.server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class SingleResponseDto<T> {
+    private T data;
+}
