2e4eda3928009db5c9ae0247f9234c7216664881
ddussi <tmqdyfud1@gmail.com>
2023-08-29 10:10:18 +0900

    rename: 필요 없는 파일 삭제 및 기본 세팅

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,18 +1,18 @@
 module.exports = {
-    extends: ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint"],
-    root: true,
-    rules: {
-      "prettier/prettier": [
-        "error",
-        {
-          printWidth: 100,
-          jsxSingleQuote: false,
-          singleQuote: false,
-          tabWidth: 2,
-          endOfLine: "auto"
-        },
-      ],
-    },
-  };
+  extends: ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        printWidth: 100,
+        jsxSingleQuote: false,
+        singleQuote: false,
+        tabWidth: 2,
+        endOfLine: "auto",
+      },
+    ],
+  },
+};
