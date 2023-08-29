1fbe1cfb7d0be42a861778e797d5b8fd576a1831
Park jungwoo <pjw951230@gmail.com>
2023-08-29 18:14:19 +0900

    Feat: OAuth 로그인 구현(카카오,깃허브,구글)

--- server/src/main/java/com/pathfinder/server/auth/oauth/MemberProfile.java
+++ server/src/main/java/com/pathfinder/server/auth/oauth/MemberProfile.java
@@ -1,13 +1,13 @@
-//package com.pathfinder.server.auth.oauth;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Getter;
-//
-//@Getter
-//@Builder
-//@AllArgsConstructor
-//public class MemberProfile {
-//
-//    private String email;
-//}
+package com.pathfinder.server.auth.oauth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class MemberProfile {
+
+    private String email;
+}
--- server/src/main/java/com/pathfinder/server/auth/utils/AuthConstant.java
+++ server/src/main/java/com/pathfinder/server/auth/utils/AuthConstant.java
@@ -21,12 +21,12 @@ public class AuthConstant {
     public static long ACCESS_TOKEN_EXPIRE_TIME;
     public static long REFRESH_TOKEN_EXPIRE_TIME;
 
-    @Value("Access Token 만료 시간 설정 예정")
+    @Value("${jwt.access-token-expire-time}")
     public void setAccessTokenExpireTime(long value) {
         ACCESS_TOKEN_EXPIRE_TIME = value;
     }
 
-    @Value("Refresh Token 만료 시간 설정 예정")
+    @Value("${jwt.refresh-token-expire-time}")
     public void setRefreshTokenExpireTime(long value) {
         REFRESH_TOKEN_EXPIRE_TIME = value;
     }
