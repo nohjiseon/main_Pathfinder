a09f022ee5d10680f1ddb22015ecafc1713b6849
nojiseon16 <nojiseon16@nojiseon16ui-MacBookPro.local>
2023-08-29 16:11:00 +0900

    feat: 초기폴더

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Footer = () => {
+  return <FooterCon>푸터</FooterCon>;
+};
+export default Footer;
+
+const FooterCon = styled.footer``;
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Header = () => {
+  return <HeaderCon>헤더</HeaderCon>;
+};
+export default Header;
+
+const HeaderCon = styled.header``;
