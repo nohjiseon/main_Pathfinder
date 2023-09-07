7877788d7280550fdb76d5a5f059dfca55caa147
Park jungwoo <pjw951230@gmail.com>
2023-09-07 16:41:17 +0900

    Fix: http://localhost:3000 허용

--- server/src/main/java/com/pathfinder/server/auth/SecurityConfig.java
+++ server/src/main/java/com/pathfinder/server/auth/SecurityConfig.java
@@ -94,6 +94,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin(frontBaseUrl);
         configuration.addAllowedOrigin("http://localhost:8080");
+        configuration.addAllowedOrigin("http://localhost:3000");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
