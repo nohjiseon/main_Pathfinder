be9c714b097f48865517c263913d1cf5633fafa7
minyongP <86906386+Minon98@users.noreply.github.com>
2023-09-15 15:31:03 +0900

    Refactoring: Delete postMemberMapper

--- server/src/main/java/com/pathfinder/server/member/mapper/MemberMapper.java
+++ server/src/main/java/com/pathfinder/server/member/mapper/MemberMapper.java
@@ -7,7 +7,6 @@ import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface MemberMapper {
-    Member memberPostToMember(MemberDto.Post requestBody);
     Member memberPatchToMember(MemberDto.Patch requestBody);
     MemberDto.Response memberToMemberResponse(Member member);
 }
