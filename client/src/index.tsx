0ed152e64ce06bdfa62a2a40c5ebab0d0d92bfda
ddussi <tmqdyfud1@gmail.com>
2023-09-15 15:39:27 +0900

    feat: 에러페이지 구현 및 렌더링 오류 수정

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,11 +5,7 @@ import "./assets/css/Reset.css";
 import axios from "axios";
 axios.defaults.baseURL = "http://ec2-43-202-120-133.ap-northeast-2.compute.amazonaws.com:8080";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- client/src/pages/Detail.tsx
+++ client/src/pages/Detail.tsx
@@ -20,7 +20,6 @@ const Detail = (): JSX.Element => {
     diaryDetailState,
     `diary/${params.id}`,
   );
-  console.log(data);
   const patchBtnHandler = () => {
     // "수정" 버튼을 클릭할 때 글쓰기/편집 페이지로 이동하도록 합니다.
     // diaryId는 수정할 일기의 ID입니다.-
@@ -64,7 +63,7 @@ const Detail = (): JSX.Element => {
     return <Loading />;
   }
   if (isError) {
-    return <DetailBg>Error...!</DetailBg>;
+    navigate("/diary/error");
   }
   return (
     <DetailBg>
