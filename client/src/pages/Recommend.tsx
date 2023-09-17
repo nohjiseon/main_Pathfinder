66bd9254d709291a51013651fd726ca03be773c9
nojiseon16 <nojiseon16@nojiseon16ui-MacBookPro.local>
2023-09-17 18:29:14 +0900

    feat: 랜덤여행지 추천

--- client/src/pages/Recommend.tsx
+++ client/src/pages/Recommend.tsx
@@ -2,50 +2,96 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { styled } from "styled-components";
 import { DiaryData } from "../types/types";
+import { RandomData } from "../types/types";
 import SubWave from "../components/common/SubWave";
 import RecommendCard from "../components/common/RecommendCard";
+import RandomCard from "../components/RandomCard";
 import Loading from "../components/common/Loading";
 import AirPlane from "../assets/images/ic_air.png";
+import BackCharacter from "../assets/images/character_02.png";
 
 const Recommend = (): JSX.Element => {
   const [recommendData, setRecommendData] = useState<DiaryData[]>([]);
+  const [randomData, setRandomData] = useState<RandomData[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
 
-  const fetchData = async () => {
-    try {
-      const res = await axios.get(
-        "http://ec2-43-202-120-133.ap-northeast-2.compute.amazonaws.com:8080/diary/recommend",
-      );
-      setRecommendData(res.data.data);
-      setIsLoading(false);
-    } catch (error) {
-      console.error("데이터 불러오기 실패:", error);
-      setIsLoading(false);
-      setIsError(true);
-    }
-  };
+  // 추천순 3위
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          "http://ec2-43-202-120-133.ap-northeast-2.compute.amazonaws.com:8080/diary/recommend",
+        );
+        setRecommendData(res.data.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("데이터 불러오기 실패:", error);
+        setIsLoading(false);
+        setIsError(true);
+      }
+    };
     fetchData();
   }, []);
+
+  // 랜덤 추천 여행지
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const address = recommendData.length > 0 ? recommendData[0].area2 : "부산";
+        const res = await axios.get(
+          `http://ec2-43-202-120-133.ap-northeast-2.compute.amazonaws.com:8080/api?address=${address}`,
+        );
+        setRandomData(res.data.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("데이터 불러오기 실패:", error);
+        setIsLoading(false);
+        setIsError(true);
+      }
+    };
+    fetchData();
+  }, [recommendData]);
 
   return (
     <MainCon>
       <SubWave />
       <RecommendCon>
-        {/* <Title>이번 여행지는 {recommendData[0].area2} 어떠세요?</Title> */}
-        <SubTitle>추천 수 가장 높은 게시물</SubTitle>
-        {isLoading ? (
-          <Loading />
-        ) : isError ? (
-          <div>server Error...</div>
-        ) : (
-          <ul>
-            {recommendData.map((item: DiaryData) => (
-              <RecommendCard key={item.diaryId} data={item}></RecommendCard>
-            ))}
-          </ul>
-        )}
+        <RecommendBox>
+          <img src={BackCharacter} className="character" />
+          <Title>
+            이번 여행지는 {recommendData.length > 0 ? recommendData[0].area2 : "어딘가"} 어떠세요?
+          </Title>
+          <SubTitle>추천 수 가장 높은 게시물</SubTitle>
+          {isLoading ? (
+            <Loading />
+          ) : isError ? (
+            <div>server Error...</div>
+          ) : (
+            <ul>
+              {recommendData.map((item: DiaryData) => (
+                <RecommendCard key={item.diaryId} data={item}></RecommendCard>
+              ))}
+            </ul>
+          )}
+        </RecommendBox>
+
+        <RandomCon>
+          <Title>
+            랜덤으로 떠나는 {recommendData.length > 0 ? recommendData[0].area2 : "어딘가"} 여행
+          </Title>
+          {isLoading ? (
+            <Loading />
+          ) : isError ? (
+            <div>server Error...</div>
+          ) : (
+            <ul>
+              {randomData.map((item: RandomData, idx: number) => (
+                <RandomCard key={idx} data={item}></RandomCard>
+              ))}
+            </ul>
+          )}
+        </RandomCon>
       </RecommendCon>
     </MainCon>
   );
@@ -70,6 +116,17 @@ const RecommendCon = styled.div`
   box-shadow: 2px 4px 6px rgba(167, 167, 167, 0.15);
   border: 0.5px solid #eaeaea;
 `;
+
+const RecommendBox = styled.div`
+  position: relative;
+  .character {
+    position: absolute;
+    right: 20px;
+    bottom: -50px;
+    opacity: 0.5;
+  }
+`;
+
 const Title = styled.strong`
   display: block;
   padding-bottom: 10px;
@@ -89,4 +146,15 @@ const Title = styled.strong`
 const SubTitle = styled.p`
   font-size: 20px;
   padding-bottom: 20px;
+`;
+
+const RandomCon = styled.div`
+  margin-top: 50px;
+  border-top: 1px solid #eaeaea;
+  padding-top: 70px;
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+  }
 `;
