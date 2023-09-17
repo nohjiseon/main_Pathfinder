72dbfef9afb3af7dd4b37e6ff79c275b6a75039c
minyongP <86906386+Minon98@users.noreply.github.com>
2023-09-17 19:00:28 +0900

    Feat: Add image file validation
    이미지 파일 이름의 마지막에
    .("jpg", "jpeg", "png", "gif", "bmp")
    로 끝나야 가능합니다.

--- server/src/main/java/com/pathfinder/server/image/service/ImageService.java
+++ server/src/main/java/com/pathfinder/server/image/service/ImageService.java
@@ -2,11 +2,10 @@ package com.pathfinder.server.image.service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.pathfinder.server.global.exception.imageexception.FileIsNotImageFile;
 import com.pathfinder.server.global.exception.imageexception.ImageNotFoundException;
-import com.pathfinder.server.global.exception.memberexception.MemberNotFoundException;
 import com.pathfinder.server.image.entity.Image;
 import com.pathfinder.server.image.repository.ImageRepository;
-import com.pathfinder.server.member.entity.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -14,6 +13,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -21,6 +22,9 @@ import java.util.UUID;
 @Component
 @Service
 public class ImageService {
+    private static final List<String> ALLOWED_IMAGE_EXTENSIONS = Arrays.asList(
+            "jpg", "jpeg", "png", "gif", "bmp"
+    );
     private final AmazonS3 amazonS3;
     private final ImageRepository imageRepository;
 
@@ -32,14 +36,18 @@ public class ImageService {
         this.imageRepository = imageRepository;
     }
 
-    public String saveFile(MultipartFile multipartFile) throws IOException {
-        String imageName = multipartFile.getOriginalFilename() + "_" + UUID.randomUUID().toString();
+    public String saveFile(MultipartFile file) throws IOException {
+        if (!isImageFile(file)) {
+            throw new FileIsNotImageFile();
+        }
+
+        String imageName = file.getOriginalFilename() + "_" + UUID.randomUUID().toString();
 
         ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(multipartFile.getSize());
-        metadata.setContentType(multipartFile.getContentType());
+        metadata.setContentLength(file.getSize());
+        metadata.setContentType(file.getContentType());
 
-        amazonS3.putObject(bucket, imageName, multipartFile.getInputStream(), metadata);
+        amazonS3.putObject(bucket, imageName, file.getInputStream(), metadata);
         String url = amazonS3.getUrl(bucket, imageName).toString();
 
         Image image = new Image();
@@ -57,12 +65,31 @@ public class ImageService {
         imageRepository.delete(image);
     }
 
-    public Image findVerifiedImage(String imageUrl) {
+    private Image findVerifiedImage(String imageUrl) {
         Optional<Image> optionalImage =
                 imageRepository.findByUrl(imageUrl);
         Image findImage =
                 optionalImage.orElseThrow(() ->
                         new ImageNotFoundException());
         return findImage;
+    }
+
+    private boolean isImageFile(MultipartFile file) {
+        String fileName = file.getOriginalFilename();
+        if (fileName == null || fileName.isEmpty()) {
+            throw new FileIsNotImageFile();
+        }
+
+        String fileExtension = getFileEctension(fileName).toLowerCase();
+        return ALLOWED_IMAGE_EXTENSIONS.contains(fileExtension);
+
+    }
+
+    private String getFileEctension(String fileName) {
+        int lastIndexOfDot = fileName.lastIndexOf(".");
+        if (lastIndexOfDot == -1) {
+            return "";
+        }
+        return fileName.substring(lastIndexOfDot+1);
     }
 }
